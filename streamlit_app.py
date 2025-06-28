a5ddb959dfa2b620160c92dc40564c34adfaac85
ivylion25 <haeri.kim@tocomail.com>
2025-06-28 18:44:36 +0000

    i finished it??

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-st.line_chart(chart_data)
+tree_height = [1.2, 1.3, 1.3, 1.3, 1.4, 1.6, 1.9, 1.9, 1.95]
+daily_rain = [.2, .1, 0, 0, .1, .2, .3, 0, .05]
+n = pd.DataFrame()
+n["tree height"] = tree_height
+n["rain"] = daily_rain
+st.title("Tree height by daily Rain")
+st.line_chart(n, x_label="days", y_label="inches")
