06050104ad1630384288705b998d571b5328cfff
ivylion25 <haeri.kim@tocomail.com>
2025-07-05 18:58:07 +0000

    started

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,27 @@
 import streamlit as st
-import pandas as pd
+import cv2
+import numpy as np
 
-tree_height = [1.2, 1.3, 1.3, 1.3, 1.4, 1.6, 1.9, 1.9, 1.95]
-daily_rain = [.2, .1, 0, 0, .1, .2, .3, 0, .05]
-n = pd.DataFrame()
-n["tree height"] = tree_height
-n["rain"] = daily_rain
-st.title("Tree height by daily Rain")
-st.line_chart(n, x_label="days", y_label="inches")
+#take = st.camera_input("Take a picture")
+
+
+#if take is not None:
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+    # Check the type of cv2_img:
+    # Should output: <class 'numpy.ndarray'>
+    cv2_img = cv2.flip(cv2_img, 1)
+    color_converted = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+
+
+    # Check the shape of cv2_img:
+    # Should output shape: (height, width, channels)
+    st.write(cv2_img.shape)
+    st.image(color_converted)
+
